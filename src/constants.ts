e6bbfd98dbcf4267d73b1f3728d474117c7da320
stefanygeraldine <stefanygeraldine@gmail.com>
2025-04-02 15:00:17 -0300

    Updates

--- src/constants.ts
+++
@@ -1,7 +0,0 @@
-export const steps = 10;
-export const height = 100;
-export const floorPosition = 570;
-export const maxHeightJump = 100;
-export const maxSpeed = 3;
-export const incrementSpeed = 1;
-export const jumpSpeed = 4;
